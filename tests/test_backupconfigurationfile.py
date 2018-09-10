ba23889a4577480c6d08e9ec4512b027218d691e
Thomas Conte <thomas@conte.com>
2018-09-10 11:41:15 +0200

    pylint

--- tests/test_backupconfigurationfile.py
+++ tests/test_backupconfigurationfile.py
@@ -1,15 +1,20 @@
+"""Unit tests for backupconfigurationfile."""
 import unittest
 from azfilebak import backupconfigurationfile
 
 class TestBackupConfigurationFile(unittest.TestCase):
+    """Unit tests for class BackupConfigurationFile."""
 
     def test_read_key_value_file(self):
-        values = backupconfigurationfile.BackupConfigurationFile.read_key_value_file(filename="config.txt.template")
+        """test read_key_value_file"""
+        values = backupconfigurationfile.BackupConfigurationFile.read_key_value_file(
+            filename="config.txt.template")
         self.assertEqual(values['local_temp_directory'], '/tmp')
         self.assertEqual(values['azure.storage.account_name'], 'sadjfksjlahfkj')
         self.assertEqual(values['azure.storage.container_name'], 'immutab')
 
     def test_get_value(self):
+        """test get_value"""
         config = backupconfigurationfile.BackupConfigurationFile(filename="config.txt.template")
         self.assertEqual(config.get_value('local_temp_directory'), '/tmp')
         self.assertEqual(config.get_value('azure.storage.account_name'), 'sadjfksjlahfkj')
