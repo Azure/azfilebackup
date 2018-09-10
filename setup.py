62a7e420b548ebadc8ac99401f235daf37478d69
Thomas Conte <thomas@conte.com>
2018-09-10 16:11:54 +0200

    azure-storage version bump to 1.3.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     },
     install_requires=[
         'pid>=2.2.0',
-        'azure-storage-common>=1.2.0rc0,<1.3.0',
-        'azure-storage-blob>=1.1.0rc0,<1.3.0',
+        'azure-storage-common>=1.3.0',
+        'azure-storage-blob>=1.3.0',
         'msrestazure>=0.4.14'
     ])
