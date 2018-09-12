877a07b62233f60571be7d92fe2ff67a80b2d0e2
Thomas Conte <thomas@conte.com>
2018-09-12 13:40:54 +0200

    Run backup command

--- azfilebak/executableconnector.py
+++ azfilebak/executableconnector.py
@@ -1,14 +1,35 @@
 # coding=utf-8
+"""
+ExecutableConnector
+"""
 
+import shlex
 import subprocess
-import os
 import logging
 
 from .naming import Naming
 from .funcmodule import printe, out, log_stdout_stderr
 from .backupexception import BackupException
 
-class ExecutableConnector:
+class ExecutableConnector(object):
+    """Drive the command that executes the backup."""
+
     def __init__(self, backup_configuration):
         self.backup_configuration = backup_configuration
 
+    def create_backup(self, fileset, is_full):
+        """Create a backup for a given fileset."""
+
+        # Determine command arguments from configuration
+        command = self.backup_configuration.get_backup_command(fileset)
+        args = shlex.split(command)
+
+        logging.debug("Executing %s", args[0])
+
+        proc = subprocess.Popen(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        return proc
---
+++ tests/test_executableconnector.py
@@ -0,0 +1,20 @@
+"""Unit tests for executableconnector."""
+import unittest
+from azfilebak import backupconfiguration
+from azfilebak import executableconnector
+
+class TestExecutableConnector(unittest.TestCase):
+    """Unit tests for class ExecutableConnector."""
+
+    def setUp(self):
+        self.cfg = backupconfiguration.BackupConfiguration(config_filename="config.txt")
+        self.connector = executableconnector.ExecutableConnector(self.cfg)
+
+    def test_create_backup(self):
+        """Test create backup."""
+        proc = self.connector.create_backup(fileset='tmp_dir', is_full=True)
+        proc.wait()
+        self.assertEqual(proc.returncode, 0)
+
+if __name__ == '__main__':
+    unittest.main()
