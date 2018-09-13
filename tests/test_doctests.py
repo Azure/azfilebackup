d21264ea0a42248bc13d5ab40a383580baeca2ab
Thomas Conte <thomas@conte.com>
2018-09-13 15:20:51 +0200

    Run doctests as part of unit tests

---
+++ tests/test_doctests.py
@@ -0,0 +1,21 @@
+import unittest
+import doctest
+from azfilebak import azurevminstancemetadata
+from azfilebak import backupconfiguration
+from azfilebak import backupconfigurationfile
+from azfilebak import businesshours
+from azfilebak import naming
+from azfilebak import scheduleparser
+from azfilebak import timing
+from azfilebak import backupagent
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(azurevminstancemetadata))
+    #tests.addTests(doctest.DocTestSuite(backupconfiguration))
+    tests.addTests(doctest.DocTestSuite(backupconfigurationfile))
+    tests.addTests(doctest.DocTestSuite(businesshours))
+    tests.addTests(doctest.DocTestSuite(naming))
+    tests.addTests(doctest.DocTestSuite(scheduleparser))
+    #tests.addTests(doctest.DocTestSuite(timing))
+    tests.addTests(doctest.DocTestSuite(backupagent))
+    return tests
