577f7ba8e674a535fd7f621ba64d0abac2abc3c5
Thomas Conté <199027+tomconte@users.noreply.github.com>
2019-05-22 14:15:42 +0200

    Increment version number (#27)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name = 'azfilebak',
-    version = '1.0-beta2',
+    version = '1.0-beta3',
     packages = ['azfilebak'],
     description="A backup utility for file systems into Azure blob storage",
     author="Microsoft Corporation",
