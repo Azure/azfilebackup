22ddbc37fc508f8e1da48ee1ae4285ff65d0d7c9
mkoderer <marc@koderer.com>
2020-06-12 10:51:54 +0200

    Add option to rate limit backup jobs (#40)
    Depending on volume size small NFS volumes in Azure can be slow (rate
    limiting).  E.g. 100gb on ANF premium has 6.4 MB/s. The backup can saturate
    this easily and this can cause outages. This patch intoduces a parameter
    to rate limit the backup read and write bandwidth using the `pv` command.
    
    Signed-off-by: Marc Koderer <marc.koderer@sap.com>
    
    Co-authored-by: Thomas Conté <199027+tomconte@users.noreply.github.com>

--- azfilebak/executableconnector.py
+++ azfilebak/executableconnector.py
@@ -13,6 +13,7 @@ import shlex
 import subprocess
 import logging
 import psutil
+import os
 
 class ExecutableConnector(object):
     """Drive the command that executes the backup."""
@@ -20,7 +21,7 @@ class ExecutableConnector(object):
     def __init__(self, backup_configuration):
         self.backup_configuration = backup_configuration
 
-    def assemble_backup_command(self, sources, exclude):
+    def assemble_backup_command(self, sources, exclude, rate = None):
         """Assemble backup command line from configuration."""
 
         # Base command
@@ -53,7 +54,15 @@ class ExecutableConnector(object):
 
         return cmd
 
-    def run_backup_command(self, command):
+    def check_pv_installed(self):
+        for path in os.environ["PATH"].split(os.pathsep):
+            pv = os.path.join(path, "pv")
+            if os.path.isfile(pv) and os.access(pv, os.X_OK):
+                return True
+        logging.warn("Rate limit set but pv cannot be found! Skipping rate limit")
+        return False
+
+    def run_backup_command(self, command, rate=None):
         """Create a backup for a given fileset."""
         args = shlex.split(command)
 
@@ -63,5 +72,9 @@ class ExecutableConnector(object):
             args,
             stdout=subprocess.PIPE
         )
+
+        if rate is not None and rate != "0" and self.check_pv_installed():
+           proc2 = subprocess.Popen(['pv', '-L %sm' % rate], stdout=subprocess.PIPE, stdin=proc.stdout)
+           return proc2
 
         return proc
